8da2b2cabd27254ec077d4ba7f719eb4ebc0e080
DusanMosny <dusan.mosny@gmail.com>
2022-10-24 20:20:58 +0200

    Creating service

---
+++ ChuckNorris-JokeApp/src/main/java/com/projectjoke/chucknorrisjokeapp/services/JokeServiceImpl.java
@@ -0,0 +1,19 @@
+package com.projectjoke.chucknorrisjokeapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements
+    JokeService {
+private final ChuckNorrisQuotes chucknorrisQuotes;
+
+  public JokeServiceImpl() {
+    this.chucknorrisQuotes = new ChuckNorrisQuotes();
+  }
+
+  @Override
+  public String getJoke() {
+    return chucknorrisQuotes.getRandomQuote();
+  }
+}
